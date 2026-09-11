6d2fb9c78fc3e02484ad3d210479e7f2bfafdc09
troudi-malek <malek.troudi@esprit.tn>
2026-09-11 11:25:55 +0100

    deploy

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.use(cors({
   credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors({
-  origin: "https://athardashbaord.vercel.app",
-  credentials: true
-}));
+
 mongo.connect(
   process.env.MONGO_URI,
   {
