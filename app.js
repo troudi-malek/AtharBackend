d23e5b0f8c71e46c9c8c537b34bf6394fdba2db4
troudi-malek <malek.troudi@esprit.tn>
2026-09-11 15:04:38 +0100

    deploy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors({
   credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
+console.log("MONGO_URI", process.env.MONGO_URI);
 mongo.connect(
   process.env.MONGO_URI,
   {
--- config/multer.js
+++ config/multer.js
@@ -1,23 +1,9 @@
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../public/uploads');
+const storage = multer.memoryStorage();
 
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir); 
-    },
-    filename: (req, file, cb) => {
-        cb(null,file.originalname); 
-    }
+const upload = multer({
+    storage: storage
 });
-
-const upload = multer({ storage: storage });
 
 module.exports = upload;
